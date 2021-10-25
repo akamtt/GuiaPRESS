44640883572d35beb43e6c9e14a74cb392ea131c
akamtt <theusluiz0@gmail.com>
2021-10-25 18:17:04 -0300

    Armazenamento de Artigos no database

--- database/connection.js
+++ database/connection.js
@@ -2,5 +2,6 @@ const Sequelize = require('sequelize');
 const connection = new Sequelize('guiapress', 'root', '08162200', {
   host: 'localhost',
   dialect: 'mysql',
+  timezone: '-03:00',
 });
 module.exports = connection;
