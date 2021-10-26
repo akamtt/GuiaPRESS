1aab10d3ff864d6bdf6fe863a42e661fcdfde8ab
akamtt <theusluiz0@gmail.com>
2021-10-26 18:16:25 -0300

    Link da Homepage com a página de cada artigo

--- index.js
+++ index.js
@@ -27,9 +27,30 @@ app.use('/', articlesController);
 app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  Article.findAll().then((articles) => {
+  Article.findAll({
+    order: [['id', 'DESC']],
+  }).then((articles) => {
     res.render('index', { articles: articles });
   });
+});
+
+app.get('/:slug', (req, res) => {
+  var slug = req.params.slug;
+  Article.findOne({
+    where: {
+      slug: slug,
+    },
+  })
+    .then((article) => {
+      if (article != undefined) {
+        res.render('article', { article: article });
+      } else {
+        res.redirect('/');
+      }
+    })
+    .catch((err) => {
+      res.redirect('/');
+    });
 });
 
 app.listen(8080, () => {
