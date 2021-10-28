c2d69980fe996d56a3e5a8927eb55a75bffa90b6
akamtt <theusluiz0@gmail.com>
2021-10-28 19:14:09 -0300

    Página de edição de artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -52,4 +52,56 @@ router.post('/articles/delete', (req, res) => {
   }
 });
 
+router.get('/admin/articles/edit/:id', (req, res) => {
+  var id = req.params.id;
+  Article.findByPk(id)
+    .then((article) => {
+      console.log(article);
+      if (article != undefined) {
+        Category.findAll().then((categories) => {
+          res.render('admin/articles/edit', {
+            categories: categories,
+            article: article,
+          });
+        });
+      } else {
+        res.redirect('/');
+      }
+    })
+    .catch((err) => {
+      res.redirect('/');
+    });
+});
+
+router.post('/articles/update', (req, res) => {
+  var id = req.body.id;
+  var title = req.body.title;
+  var body = req.body.body;
+  var category = req.body.category;
+
+  Article.update(
+    {
+      title: title,
+      body: body,
+      categoryId: category,
+      slug: slugify(title),
+    },
+    {
+      where: {
+        id: id,
+      },
+    },
+  ).then(() => {
+    res.redirect('admin/articles');
+  });
+});
+
+/* router.get('articles/page/:num', (req, res) => {
+  var page = req.params.num;
+
+  Article.findAndCountAll().then((articles) => {
+    res.json(articles);
+  });
+}); */
+
 module.exports = router;
