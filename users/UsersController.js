2e3f3d895856a87279d53da856fbd142b7def141
akamtt <theusluiz0@gmail.com>
2021-11-04 17:15:15 -0300

    Conexão de login com database e hash de senha

--- users/UsersController.js
+++ users/UsersController.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const User = require('./User');
+const bcrypt = require('bcryptjs');
 
 router.get('/admin/users', (req, res) => {
   res.send('Listagem de usuários');
@@ -13,7 +14,25 @@ router.post('/users/create', (req, res) => {
   var email = req.body.email;
   var password = req.body.password;
 
-  res.json({ email, password });
+  User.findOne({ where: { email: email } }).then((user) => {
+    if (user == undefined) {
+      var salt = bcrypt.genSaltSync(10);
+      var hash = bcrypt.hashSync(password, salt);
+
+      User.create({
+        email: email,
+        password: hash,
+      })
+        .then(() => {
+          res.redirect('/');
+        })
+        .catch((err) => {
+          res.redirect('/');
+        });
+    } else {
+      res.redirect('/admin/users/create');
+    }
+  });
 });
 
 module.exports = router;
