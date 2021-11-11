47813a51cabfce39f56cc97e3ebb6fc532de9846
akamtt <theusluiz0@gmail.com>
2021-11-11 16:30:40 -0300

    Logout

--- users/UsersController.js
+++ users/UsersController.js
@@ -84,4 +84,10 @@ router.post('/authenticate', (req, res) => {
     }
   });
 });
+
+router.get('/logout', (req, res) => {
+  req.session.user = undefined;
+  res.redirect('/');
+});
+
 module.exports = router;
